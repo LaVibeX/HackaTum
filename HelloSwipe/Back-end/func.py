b5b9506f132f81c85c688f3c2df61a2a527b1dc3
Vayun Soni <vayunsoni@gmail.com>
2023-11-18 19:17:27 +0100

    Add POST functionality to backend, add exclusion to func.py

--- HelloSwipe/Back-end/func.py
+++ HelloSwipe/Back-end/func.py
@@ -5,56 +5,41 @@ import random
 
 preffered_url = "https://someGiorgiLink"
 ingredients_url = "https://someGiorgiLink"
-recipes_url = "../Front-end/src/recipes.json"
+recipes_url = "http://localhost:5000/recipes"
 user_url = "../Front-end/src/user.json"
 
 
 
 def get_json_to_dict(request_url):
-    #r= requests.get(request_url)
-    # Opening JSON file
-    with open(request_url) as json_file:
-        r_dict = json.load(json_file)
-    return r_dict
+    r= requests.get(request_url)
+    return r.json()
+
 
 def send_post(json_value, url):
     x = requests.post(url, json = json_value)
 
-def list_to_json(value, url):
+def list_to_json(value):
     # Convert list to JSON
-    json_data = json.dumps(value, indent=2)  
+    json_data = json.dumps(value, indent=None) 
+    json_data = json_data.replace("\\", "")
     return json_data
 
-def check_any_ingredient(choice_dict, word):
-    #boolean_list = []
-    list_recipes = []
-    for recipe in choice_dict:
-        for ingredient in recipe["recipe"]["ingredients"]:
-            #boolean_list.append([recipe["recipe"]["name"], ingredient == word])
-            if ingredient == word:
-                list_recipes.append(recipe["recipe"]["name"])
-    return list_recipes#any(boolean_list)
+def common_ingredients(recipes, user_ingredients,exclusions):
+    list_recipe = []
+    for recipe in recipes:
+        counter = 0
+        for user_ingredient in user_ingredients:
+            if user_ingredient in recipe["recipe"]["ingredients"]:
+                counter += 1
+        for exclusion in exclusions:
+            if(exclusion not in recipe["recipe"]["ingredients"]):
+                list_recipe.append([counter,recipe["recipe"]["name"]])
+        list_recipe = sorted(list_recipe, key=lambda x: x[0],reverse=True)
 
-def mock_erase_before_deploy(choice_dict, word_list):
-    boolean_list = []
-    not_in_list= []
-    for word in word_list:
-        is_ingredient = check_any_ingredient(choice_dict, word)
-        boolean_list.append(is_ingredient)
-        if not is_ingredient:
-            not_in_list.append(word)
-    print(not_in_list)
-    return all(boolean_list)
+    return list_recipe
+            
 
-def filter_from_ingredient_list(choice_dict, word_list):
-    recipe_list = []
-    final_recipe_list = []
-    for word in word_list:
-        recipe_list.append(check_any_ingredient(choice_dict, word))
-    
-    for recipe in recipe_list:
-        final_recipe_list.append(random.choice(recipe))
-    return final_recipe_list#all(boolean_list)
+
 
 def ingredients_on_cuisines(style_dict):
     ingredients_set = set()
@@ -62,8 +47,8 @@ def ingredients_on_cuisines(style_dict):
         for ingredient in recipe["recipe"]["ingredients"]:
             ingredients_set.add(ingredient)
     # Convert set to list (optional, for better readability)
-    ingredients_list = list(ingredients_set)
-    return ingredients_list
+    ingredient_list = list(ingredients_set)
+    return ingredient_list
 
 def get_cuisines(preffered_styles, recipes):
     user_choice = []
@@ -78,15 +63,18 @@ def main():
     #preffered_styles = get_json_to_dict(preffered_url)
     ingredients = ['pork', 'tomato', 'avocado'] #get_json_to_dict(ingredients_url)
     recipes = get_json_to_dict(recipes_url)
-    preffered_styles = ["Mediterranean", "Chinese", "Indian"]
+    preffered_styles = ["Mexican", "Chinese", "Indian"]
+    exclusions = ["pineapple"]
 
     user_choice = get_cuisines(preffered_styles, recipes)
+    print(list_to_json(ingredients_on_cuisines(user_choice)))
 
-    #send_post(list_to_json(ingredients_on_cuisines(user_choice)))
+
+    #send_post(list_to_json(ingredients_on_cuisines(user_choice)),'http://localhost:5000/ingredients_on_recipes')
 
 
-    print(filter_from_ingredient_list(user_choice, ingredients))
-   
+    #print(filter_from_ingredient_list(user_choice, ingredients))
+    print(common_ingredients(user_choice, ingredients,exclusions))
  
 
 if __name__ == '__main__':
--- HelloSwipe/Back-end/main.py
+++ HelloSwipe/Back-end/main.py
@@ -11,25 +11,39 @@ CORS(app)
 @app.route("/")
 # def hello():
 #     return "Hello, world"
+@app.route('/recipes', methods=['GET'])
+def get_data_2():
+    with open(r'D:\Codes\hellofresh\HackaTum\HelloSwipe\Front-end\src\recipes.json') as file:
+        json_data_1 = json.load(file)
+    return json_data_1
 
-@app.route('/get_data', methods=['GET'])
-def get_data():
-    # Logic to read data from the JSON file
+@app.route('/carousel', methods=['GET'])
+def get_data_1():
     with open('D:\Codes\hellofresh\HackaTum\HelloSwipe\Front-end\src\carousel.json', 'r') as file:
         json_data = json.load(file)
-
     # Return the JSON data as a response
     return json_data
 
-@app.route('/post-example', methods=['POST'])
+
+
+@app.route('/ingredients_on_recipes', methods=['POST'])
 def post_example():
     if request.method == 'POST':
         data = request.json  # Assuming the data is sent as JSON in the request
+        app.ingredients_on_recipes = data
         # Process the received data
         # For example, let's echo back the received data
         return jsonify(data)
     else:
         return jsonify({'message': 'This endpoint only accepts POST requests'})
+    
+@app.route('/ingredients_recipes', methods=['GET'])
+def get_ingredients():
+    if hasattr(app, 'ingredients_on_recipes'):
+      ingredients_on_recipes = app.ingredients_on_recipes  # Retrieve stored recent data
+      return jsonify(ingredients_on_recipes)
+    else:
+        return jsonify({'message': 'No recent data'})
 
 if __name__ == '__main__':
     app.run(debug=True, port = 5000)
